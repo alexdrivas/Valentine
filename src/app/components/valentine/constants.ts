419f712ac1c0930e80850d94dda02da6104adb4a
Alexandros Drivas <aadrivas@gmail.com>
2026-02-09 17:49:27 -0800

    update lines

--- src/app/components/valentine/constants.ts
+++ src/app/components/valentine/constants.ts
@@ -2,7 +2,7 @@ export const TITLE = "Danielle,";
 
 export const LINES = [
   "Somehow, in the middle of everything, you became my favorite place to be.",
-  "With you, the little moments matter bigger. The laughs, the food, the just being in the same space.",
+  "With you, the little moments matter more. The laughs, the food, the just being in the same space.",
   "I don't know when it happened, only that I'm really glad it did.",
 ];
 
